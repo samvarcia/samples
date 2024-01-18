1a51fd21cc7af826238ec687d4636889c30d1632
Samuel Garcia <samfrontdev@gmail.com>
2024-01-18 12:57:09 -0500

    i get the data version

--- pages/api/samples.js
+++ pages/api/samples.js
@@ -1,3 +1,11 @@
-export default function GetSamples(req, res){
-  res.status(200).json({samples: []})
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export default async function handler(req, res) {
+  try {
+    const samples = await sql`SELECT * FROM mediaitems;`;
+    res.status(200).json({ samples });
+  } catch (err) {
+    res.status(500).json({ error: "failed to load data" });
+  }
 }
